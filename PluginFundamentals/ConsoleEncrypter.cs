9da3f8634d3a937bb3a70b8aba025ef57f100bfb
S-1-5-21-1828239032-2288726806-2036108429-1109 <S-1-5-21-1828239032-2288726806-2036108429-1109@.(none)>
2014-05-25 22:15:08 +0200

    Changeset: 56 Changeset: 33 Create Leet Plugin, update output path for easier debuging.

---
+++ PluginFundamentals/ConsoleEncrypter.cs
@@ -0,0 +1,31 @@
+﻿using PluginFundamentals.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginFundamentals
+{
+  public class ConsoleEncrypter
+  {
+    private readonly IEncryptionAlgorithm encryptionAlgorithm;
+
+    public ConsoleEncrypter(IEncryptionAlgorithm encryptionAlgorithm)
+    {
+      this.encryptionAlgorithm = encryptionAlgorithm;
+    }
+
+    public void GetAndEncryptInput()
+    {
+      // Get input
+      string input = Console.ReadLine().ToUpper();
+
+      // "Encrypt" it
+      string output = encryptionAlgorithm.Encrypt(input);
+
+      // Write output
+      Console.WriteLine(output);
+    }
+  }
+}
